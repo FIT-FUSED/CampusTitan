f9edddb5ad52d60a0559036eecc75ae68174f7dc
devanshjain009 <devanshjain037@gmail.com>
2026-03-14 01:39:28 +0530

    table addition

--- check_dietary_column.sql
+++
@@ -1,8 +0,0 @@
--- Check if dietary_preferences column exists in users table
-SELECT column_name, data_type, column_default 
-FROM information_schema.columns 
-WHERE table_name = 'users' 
-AND column_name = 'dietary_preferences';
-
--- If no results, the column doesn't exist and you need to run the migration
--- If results appear, the column already exists
